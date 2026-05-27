8550997f5cef9e888e1b5ec9a85f57437defad26
Ushan02 <ushansanuka2023@gmail.com>
2026-05-27 13:07:56 +0530

    update env

--- pages/loginPage.jsx
+++ pages/loginPage.jsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
 
     setLoading(true);
     try {
-      const res = await axios.post("http://localhost:5000/api/users/login", {
+      const res = await axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login", {
         email,
         password,
       });
